5e850ae769ef2d43cabf1b2916c43bed8fa142a6
Ahmet Selçuk Özdemir <ahmetselcukozdemir01@gmail.com>
2023-03-27 03:27:54 +0300

    27-03-2023

---
+++ RabbitMQ/RabbitMQ.Catch/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("*********");
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("mesaj-kuyruk", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("mesaj-kuyruk", true, consumer);
+
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine("Gelen mesaj;",Encoding.UTF8.GetString(e.Body.ToArray()));
+        }
+    }
+}
---
+++ RabbitMQ/RabbitMQ.Sender/Program.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Sender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                SenderMessage();
+            }
+
+        }
+
+        public static void SenderMessage()
+        {
+            try
+            {
+                var factory = new ConnectionFactory();
+
+                //url'i tanımlıyoruz.
+                factory.Uri = new Uri("amqps://txuwvldz:4SmUuZQISipyjCoSrM81QFEhudCpZE9N@chimpanzee.rmq.cloudamqp.com/txuwvldz");
+
+                //connection oluşturuyoruz.
+
+                var connection = factory.CreateConnection();
+
+                //connection'na bir kanal yaratıyoruz.
+                var channel = connection.CreateModel();
+
+                channel.QueueDeclare("mesaj-kuyruk", true, false, false);
+
+                //gönderilecek mesajı tanımlıyoruz.
+
+                var mesaj = "Test";
+
+                //mesajı kuyruğa iletebilmek için binary formatına çeviriyoruz.
+
+                var body = Encoding.UTF8.GetBytes(mesaj);
+
+                channel.BasicPublish(String.Empty, "mesaj-kuyruk", null, body);
+
+                Console.WriteLine("Mesaj kuyruğa eklendi.");
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+    }
+}

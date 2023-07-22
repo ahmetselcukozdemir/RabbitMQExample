99331d44fc48dff94dba60ff6e0d65faaade00ac
Ahmet Selçuk Özdemir <ahmetselcukozdemir01@gmail.com>
2023-07-23 01:13:31 +0300

    23_07_2023_1_simple_level

---
+++ RabbitMQ/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BASİT DÜZEYDE BİR CONSUMER ÖRNEĞİ
+
+            SimpleLevelConsumer();
+        }
+
+        public static void SimpleLevelConsumer()
+        {
+            //öncelikle bir bağlantı oluşturuyoruz.
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://lqklsdpn:EWvh4S5aLfwruWutVwkbkeVik5OI6mkw@toad.rmq.cloudamqp.com/lqklsdpn");
+
+            //bağlantıyı aktifleştirme ve kanal açma
+
+            IConnection connection = factory.CreateConnection();
+
+            IModel channel = connection.CreateModel();
+
+            //Queue oluşturma(kuyruk oluşturma) 
+
+            channel.QueueDeclare(queue: "example-queue", exclusive: false); //ÖNEMLİ ! Consumerdaki kuyruk pusblisherdaki gibi bire bir aynı tanımlanmalıdır.
+
+            //Queue(kuyruktan) mesaj okuma
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(queue: "example-queue",false,consumer);
+            consumer.Received += (sender, e) =>
+            {
+                //kuyruğa gelen mesajın işlendiği yerdir.
+                //e.Body : bize kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+                //e.Body.Span veya e.Body.ToArray() : kuyruktaki verinin byte verisini getirecektir.
+
+                byte[] bodyData = e.Body.ToArray();
+                Console.WriteLine(Encoding.UTF8.GetString(bodyData));
+            };
+
+            Console.Read();
+        }
+    }
+}
---
+++ RabbitMQ/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //basit düzeyde bir publisher örneği
+            SimpleLevelPublisher();
+
+        }
+
+        public static void SimpleLevelPublisher()
+        {
+            //öncelikle bir bağlantı oluşturuyoruz.
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://lqklsdpn:EWvh4S5aLfwruWutVwkbkeVik5OI6mkw@toad.rmq.cloudamqp.com/lqklsdpn");
+
+            //bağlantıyı aktifleştirme ve kanal açma
+
+            IConnection connection = factory.CreateConnection();
+
+            IModel channel = connection.CreateModel();
+
+            //Queue oluşturma(kuyruk oluşturma) 
+
+            channel.QueueDeclare(queue: "example-queue", exclusive: false); //exclusive yapmamızın nedeni birden fazla bağlantıyla bu kuyrukta işlem yapıp yapamayacağımızı belirtmek için.
+
+            //Queue mesaj gönderme
+
+            //NOT! : Rabbitmq kuyruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları byte'e dönüştürmemiz gerekiyor.
+
+            #region burasını şimdilik yorum satırı yaptım.
+            //byte[] message = Encoding.UTF8.GetBytes("Merhaba !");
+
+            //channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+            #endregion
+
+            for (int i = 0; i < 100; i++)
+            {
+                Task.Delay(200); // bekletme
+
+                byte[] message = Encoding.UTF8.GetBytes("Merhaba "+i);
+
+                channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+            }
+
+            Console.Read();
+        }
+    }
+}
